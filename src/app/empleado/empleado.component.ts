49cd7b5c82342231df363a6b7cb0c78d318cba88
Kelvin Molina <kelvinmolinn@gmail.com>
2023-08-21 19:31:54 -0600

    modificacion de clase del lunes 21

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -12,13 +12,21 @@ edad:number = 15;
 empresa = 'otra';
 sector = 'otro';
 jefe = 'otros';
-
-
+habilitar_input_property = false;
+usuario_registrado_property = true;
+texto_registro="no hay nadie";
 getEdad(){
   return this.edad;
 }
 
 set_empresa(empresa: string){
 
+}
+cambiar_registro(){
+  this.usuario_registrado_property =! this.usuario_registrado_property;
+}
+set_usuario_registrado(){
+  alert('el usuario se ha registrado');
+  this.texto_registro = 'el usuario se acaba de registrar';
 }
 }
